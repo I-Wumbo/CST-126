346e43ccf3fdfa29e319682bccc2eeda5239974c
Karl Albornoz <karl.albornoz13@gmail.com>
2019-03-03 22:13:48 -0700

    Changed the table to lower case registeredusers

--- Blog/blogHandler.php
+++ Blog/blogHandler.php
@@ -2,13 +2,13 @@
 $servername= "localhost";
 $username = "root";
 $password = "root";
-$database = "Blog";
+$database = "blog";
 
 $registeredUsername = $_POST['username'];
 $registeredPassword = $_POST['password'];
 
 $conn = new mysqli($servername, $username, $password, $database);
-$sql = "INSERT INTO RegisteredUsers (USER_NAME, PASSWORD)
+$sql = "INSERT INTO registeredusers (USER_NAME, PASSWORD)
 VALUES ('$registeredUsername', '$registeredPassword')";
 
 if($conn->query($sql) === TRUE){
